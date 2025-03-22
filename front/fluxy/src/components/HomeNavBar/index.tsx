c6197799898ef1bb2b248488d8159fe74274b46d
Paulo Rosado <paulohenrique110803@gmail.com>
2025-03-21 21:10:30 -0300

    fixing back button

--- front/fluxy/src/components/HomeNavBar/index.tsx
+++ front/fluxy/src/components/HomeNavBar/index.tsx
@@ -7,7 +7,6 @@ import { MenuIcon, NavBar } from "./styles";
 export function HomeNavBar() {
 
     const [showMenu, setShowMenu] = useState(false);
-    const [cont, setCont] = useState(0);
 
     const handleShowMenu = () => {
         setShowMenu(() => {
@@ -23,19 +22,15 @@ export function HomeNavBar() {
         });
     };
 
-    const handleCloseMenuB = () => {
-        setCont(prev => prev + 1);
-    };
-
     return(
         <>
             <MenuIcon size={50} weight="bold" onClick={handleShowMenu} />
             <NavBar className={showMenu ? "active" : "deactive"}>
                 <ul className="active">
                     {showMenu &&
-                        <ArrowCircleLeft size={50} color="white" weight="bold" onClick={handleCloseMenuB} />
+                        <ArrowCircleLeft size={50} color="white" weight="bold" onClick={handleCloseMenu} />
                     }
-                    <li><a href="#about" onClick={handleCloseMenu}>Sobre {cont}</a></li>
+                    <li><a href="#about" onClick={handleCloseMenu}>Sobre</a></li>
                     <li><a href="#clients" onClick={handleCloseMenu}>Clientes</a></li>
                     <li><a href="#contact" onClick={handleCloseMenu}>Contato</a></li>
                     <li><a id="button" href="#" onClick={handleCloseMenu}>Comece agora</a></li>
