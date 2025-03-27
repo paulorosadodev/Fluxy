30e9998aab4e3408910455f6effbc4a73a446f76
n3waz <lhk@cesar.school>
2025-03-27 14:36:59 -0300

    created springboot structure back-end

---
+++ back/fluxy/src/main/java/fluxy/spring/project/FluxyApplication.java
@@ -0,0 +1,13 @@
+package fluxy.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxyApplication.class, args);
+	}
+
+}
