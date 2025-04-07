cec4470b1c19c6566f07e6e0053530be2e3ef96f
n3waz <lhk@cesar.school>
2025-04-07 19:45:31 -0300

    created new package -> auth | needs to configurated

---
+++ back/fluxy/src/main/java/br/com/project/service/auth/SecurityFilter.java
@@ -0,0 +1,47 @@
+package br.com.project.service.auth;
+
+import br.com.project.model.Loja;
+import br.com.project.repository.LojaRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    LojaRepository lojaRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        String login = tokenService.validadeToken(token);
+
+        if (login != null) {
+            Loja loja = lojaRepository.encontrarPorNome(login).orElseThrow(() -> new RuntimeException("Loja não encontrada"));
+            var authentication = new UsernamePasswordAuthenticationToken(loja, null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
---
+++ back/fluxy/src/main/java/br/com/project/service/auth/TokenService.java
@@ -0,0 +1,53 @@
+package br.com.project.service.auth;
+
+import br.com.project.model.Loja;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    public String generateToken(Loja loja) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            String token = JWT.create().withIssuer("fluxy")
+                    .withSubject(loja.getUsername())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String validadeToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            return JWT.require(algorithm).withIssuer("fluxy")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
