ca2f0fbf5470456b7ee70cfeda49cff3b048e3ff
Paulo Rosado <paulohenrique110803@gmail.com>
2025-04-18 10:59:42 -0300

    adding select, edit and delete buttons

--- front/fluxy/src/components/DataTable/index.tsx
+++ front/fluxy/src/components/DataTable/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
-import { DataTableWrapper, InputWrapper } from "./styles";
+import { useEffect, useState } from "react";
+import { DataTableWrapper, InputWrapper, ActionsWrapper } from "./styles";
 import { SearchFilterInput } from "../SearchFilterInput";
+
+import { Square, CheckSquare, Trash, Pencil } from "phosphor-react";
 
 type DataTableProps<T extends Record<string, any>> = {
     data: T[];
@@ -17,31 +19,76 @@ export type Column<T> = {
 export function DataTable<T extends Record<string, any>>({ data, columns, entityName }: DataTableProps<T>) {
 
     const [filteredData, setFilteredData] = useState<any[]>([]);
+    const [selectedsRow, setSelectedsRow] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (selectedsRow) {
+            const filteredRowStrings = filteredData.map(row => Object.values(row).toString());
+    
+            const updatedSelecteds = selectedsRow.filter(rowStr =>
+                filteredRowStrings.includes(rowStr)
+            );
+    
+            if (updatedSelecteds.length !== selectedsRow.length) {
+                setSelectedsRow(updatedSelecteds.length > 0 ? updatedSelecteds : []);
+            }
+        }
+    }, [selectedsRow, filteredData]);
+
+    function handleSelect(row: string | undefined) {
+        if (!row) return;
+    
+        setSelectedsRow(prev => {
+            const current = prev ?? [];
+    
+            if (current.includes(row)) {
+                return current.filter(item => item !== row);
+            } else {
+                return [...current, row];
+            }
+        });
+    }    
 
     return (
         <>
             <InputWrapper>
-                <button>Adicionar</button>
+                <ActionsWrapper>
+                    <button>Adicionar</button>
+                    <Pencil id="edit" className={selectedsRow.length === 1 ? "active" : ""} size={25} />
+                    <Trash id="delete" className={selectedsRow.length >= 1 ? "active" : ""} size={25} />
+                </ActionsWrapper>
                 <SearchFilterInput data={data} columns={columns} filteredData={filteredData} setFilteredData={setFilteredData} entityName={entityName}/>
             </InputWrapper>
             <DataTableWrapper>
                 <table className="responsive-table">
                     <thead>
                         <tr>
+                            <th></th>
                             {columns.map((column, index) => (
                                 <th scope="col" key={index}>{column.header}</th>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
-                        {filteredData.map((row, rowIndex) => (
-                            <tr key={rowIndex}>
+                        {filteredData.map((row) => (
+                            <tr key={Object.values(row).toString()} onClick={() => handleSelect(Object.values(row).toString())}>
+                                <td className={`selector ${selectedsRow?.includes(Object.values(row).toString()) ? "selected" : ""}`}>
+                                    <ActionsWrapper className="mobile inactive">
+                                        <Pencil id="edit" className={selectedsRow ? "active" : ""} size={25} />
+                                        <Trash id="delete" className={selectedsRow ? "active" : ""} size={25} />
+                                    </ActionsWrapper>
+                                    {selectedsRow?.includes(Object.values(row).toString()) ?
+                                        <CheckSquare className="square" weight="bold" width={16} height={18}/>
+                                        :
+                                        <Square className="square" weight="bold" width={16} height={18}/>
+                                    }
+                                </td>
                                 {columns.map((column, colIndex) => {
                                     const cellValue = row[column.accessor];
                                     const formattedValue = column.formatter
                                         ? column.formatter(cellValue) ?? "?"
                                         : cellValue ?? "?";
-                                    return <td data-label={columns[colIndex].header + ":"} key={colIndex}>{formattedValue}</td>;
+                                    return <td className={selectedsRow?.includes(Object.values(row).toString()) ? "selected" : ""}  data-label={columns[colIndex].header + ":"} key={colIndex}>{formattedValue}</td>;
                                 })}
                             </tr>
                         ))}
--- front/fluxy/src/components/DataTable/styles.ts
+++ front/fluxy/src/components/DataTable/styles.ts
@@ -7,7 +7,21 @@ export const InputWrapper = styled.div`
     margin-top: 1.5rem;
     margin-bottom: 1rem;
 
-    & > button {
+    @media (max-width: 880px) {
+        flex-direction: column-reverse;
+
+        button {
+            width: 100%;
+        }
+    }
+`;
+
+export const ActionsWrapper = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    button {
         padding: 0.75rem 1rem;
         border-radius: 8px;
         font-size: 1rem;
@@ -17,20 +31,66 @@ export const InputWrapper = styled.div`
         font-weight: bold;
         border: none;
         transition: all .2s;
+        margin-right: 0.5rem;
     }
     
-    & > button:hover {
+    button:hover {
         background-color: ${(props) => props.theme["purple-200"]};
         cursor: pointer;
     }
 
-    @media (max-width: 880px) {
-        flex-direction: column-reverse;
+    svg {
+        margin-right: 0.2rem;
+        color: ${(props) => props.theme["gray-200"]};
+    }
 
+    svg.active {
+        cursor: pointer;
+    }
+    
+    #edit.active {
+        color: ${(props) => props.theme["purple-400"]};
+    }
+
+    #delete.active {
+        color: ${(props) => props.theme["orange-500"]};
+    }
+
+    &.mobile {
+        display: none;
+    }
+
+    @media (max-width: 880px) {
+        width: 100%;
+        
         button {
-            width: 100%;
+            margin: 0;
+        }
+
+        svg {
+            display: none;
+        }
+
+        &.mobile {
+
+            display: flex;
+
+            svg {
+                display: block;
+                width: 1.5rem;
+                height: 1.5rem;
+            }
+
+            #edit {
+                color: ${(props) => props.theme["purple-400"]};
+            }
+
+            #delete {
+                color: ${(props) => props.theme["orange-500"]};
+            }
         }
     }
+
 `;
 
 export const DataTableWrapper = styled.div`
@@ -60,6 +120,10 @@ export const DataTableWrapper = styled.div`
         border: 1px solid #ddd;
     }
 
+    td.selected {
+        background-color: ${(props) => props.theme["gray-50"]};
+    }
+
     thead tr {
         background-color: ${(props) => props.theme["purple-300"]};
         color: ${(props) => props.theme["white"]};
@@ -69,12 +133,23 @@ export const DataTableWrapper = styled.div`
         background-color: ${(props) => props.theme["white"]};
     }
 
+    .selector {
+        svg {
+            width: 100%;
+        }
+
+        svg:hover {
+            cursor: pointer;
+        }
+    }
+
     @media (max-width: 880px) {
 
+        
         .responsive-table {
             border: 0;
         }
-
+        
         .responsive-table caption {
             font-size: 1.3rem;
         }
@@ -89,34 +164,55 @@ export const DataTableWrapper = styled.div`
             position: absolute;
             width: 1px;
         }
-
+        
         .responsive-table tr {
             border-bottom: 3px solid ${(props) => props.theme["gray-200"]};
             display: block;
             margin-bottom: 0.625rem;
             padding: 0;
         }
-
+        
         .responsive-table td {
             border-bottom: 1px solid ${(props) => props.theme["gray-200"]};
             display: block;
             font-size: 1rem;
             text-align: right;
         }
-
+        
         .responsive-table td::before {
             content: attr(data-label);
             float: left;
             font-weight: 700;
             font-size: 0.9rem;
         }
-
+        
         .responsive-table td:last-child {
             border-bottom: 0;
         }
-
+        
         tr:nth-child(even) {
             background-color: white;
+        }
+
+        td.selected {
+            background-color: white;
+        }
+
+        .selector {
+
+
+            div {
+                justify-content: flex-end;
+            }
+
+            svg {
+                width: auto;
+            }
+
+            .square {
+                display: none;
+            }
+
         }
     }
 `;
